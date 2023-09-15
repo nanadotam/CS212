14034a7cc6d66eb601a6c4219561b213f06fefa0
nanaamoako <63524504+nanadotam@users.noreply.github.com>
2023-09-15 23:37:06 +0000

    Update palindrome.py
    - Shorter code
    - Easy-to-read comments

--- palindrome.py
+++ palindrome.py
@@ -1,31 +1,27 @@
-def is_palindrome(word):
-    # Base case: If the word is empty or contains only one character, it's a palindrome.
+# Recursive function that checks if a word is a palindrome or not
+def is_paindrome(word):
     if len(word) <= 1:
         return True
     
-    # Check if the first and last characters of the word are the same.
     if first(word) == last(word):
-        # Recursively check the middle part of the word.
-        return is_palindrome(middle(word))
+        return is_paindrome(middle(word))
     
-    # If the first and last characters are different, it's not a palindrome.
-    return False
-
-# Helper function to get the first character of a string.
+    else:
+        return False
+    
+# Helper functions to obtain the first, last, and middle characters of a string
 def first(word):
     return word[0]
 
-# Helper function to get the last character of a string.
 def last(word):
     return word[-1]
 
-# Helper function to get the middle part of a string (excluding the first and last characters).
 def middle(word):
     return word[1:-1]
 
-# Example usage:
-word = "racecar"
-if is_palindrome(word):
+# Usage
+word = input("Enter a word to check if it's a palindrome:\n -> ")
+if is_paindrome(word):
     print(f"{word} is a palindrome.")
 else:
     print(f"{word} is not a palindrome.")
